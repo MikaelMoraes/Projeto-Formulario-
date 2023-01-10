9dc436eaa42aaa2634acfc867c39e0983b732069
Mikael Vitot <68564996+MikaelMoraes@users.noreply.github.com>
2023-01-10 15:07:45 -0300

    armazenamento de informação, resete e envio

---
+++ src/components/Formulario.vue
@@ -0,0 +1,290 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 bg-light">
+                <span class="fs-4">ENTRADA DE DADOS</span>
+                <hr>
+                <form @submit="enviar($event)" @reset.prevent="resetar()">
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Nome:</label>
+                        <div class="col">
+                            <input type="text" class="form-control " v-model="form.nome">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">E-mail:</label>
+                        <div class="col">
+                            <input type="email" class="form-control " v-model="form.email">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Senha:</label>
+                        <div class="col">
+                            <input type="password" class="form-control " v-model="form.senha">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Idade:</label>
+                        <div class="col">
+                            <input type="number" class="form-control" v-model="form.idade">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Gênero:</label>
+                        <div class="col">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" value="Feminino" v-model="form.genero">
+                                <label class="form-check-label">
+                                    Feminino
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" value="Masculino" v-model="form.genero">
+                                <label class="form-check-label">
+                                    Masculino
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Licença:</label>
+                        <div class="col">
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" 
+                                 type="checkbox"
+                                 v-model="form.licenca"
+                                 true-value="SIM"
+                                 false-value="NÃO">
+                                <label class="form-check-label">Li e aceito os termos</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Interesses:</label>
+                        <div class="col">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="javascript" v-model="form.interesses">
+                                <label class="form-check-label">
+                                    JavaScript
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="VueJs" v-model="form.interesses">
+                                <label class="form-check-label">
+                                    VueJS
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="Angular" v-model="form.interesses">
+                                <label class="form-check-label">
+                                    Angular
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="NodeJS" v-model="form.interesses">
+                                <label class="form-check-label">
+                                    NodeJS
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Telefone:</label>
+                        <div class="col">
+                            <input type="text" class="form-control" v-maska="'(##) #####-####'" v-model="form.telefone">
+                            <small class="text-muted">Formato: 11 97777-5555</small>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Data:</label>
+                        <div class="col">
+                            <input type="date" class="form-control">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Data/hora local:</label>
+                        <div class="col">
+                            <input type="datetime-local" class="form-control" v-model="form.dataHoraLocal"> 
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Mês:</label>
+                        <div class="col">
+                            <input type="month" class="form-control" v-model="form.mes">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Semana:</label>
+                        <div class="col">
+                            <input type="week" class="form-control" v-model="form.semana">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Hora:</label>
+                        <div class="col">
+                            <input type="time" class="form-control" v-model="form.hora">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Cor:</label>
+                        <div class="col">
+                            <input type="color" class="form-color" v-model="form.cor">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Valor limite:</label>
+                        <div class="col">
+                            <input type="range" class="form-range" min="0" max="100" step="1" v-model="form.alcance">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Escondido:</label>
+                        <div class="col">
+                            <input type="hidden" class="form-control" v-model="form.escondido">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Upload:</label>
+                        <div class="col">
+                            <input type="file" class="form-control" multiple @change="selecionarArquivos($event)">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="mb-3 row">
+                        <div class="col d-flex justify-content-between">
+                            <button class="btn btn-secondary" type="reset">Limpar</button>
+                            <button class="btn btn-success" type="button">Enviar (btn)</button>
+                            <button class="btn btn-success" type="submit">Enviar (submit)</button>
+                        </div>                        
+                    </div>
+                   
+                </form>
+            </div>
+
+            
+            <div class="col-6 text-white bg-secondary" :style="'background-color:'+form.cor+'!important'">
+                <span class="fs-4">ESTADO DO OBJETO</span>
+                <hr>
+                <div class="mb-5 row">
+                    <span>{{form}}</span>
+                </div>
+
+                <span class="fs-4">SAÍDA DE DADOS</span>
+                <hr>
+                <div class="mb-3 row">
+                    <span>Nome: {{form.nome}}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>E-mail: {{form.email}}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Senha: {{form.senha}}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Idade: {{ form.idade }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Gênero: {{ form.genero}}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Licença: {{ form.licenca }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Interesses:</span>
+                    <ul>
+                        <li v-for="(interesses, index) in form.interesses" :key="index">
+                        {{ interesses }}
+                        </li>
+                    </ul>
+                </div>
+                <div class="mb-3 row">
+                    <span>Telefone: {{form.telefone}}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Data:</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Data/hora local: {{ form.dataHoraLocal }}</span>
+                    <ul>
+                        <li>{{ $moment(form.dataHoraLocal).format('dddd') }}</li>
+                    </ul>
+                </div>
+                <div class="mb-3 row">
+                    <span>Mês: {{ form.mes }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Semana: {{ form.semana }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Hora: {{ form.hora }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Cor: {{ form.cor }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Valor limite: {{ form.alcance }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Escondido: {{ form.escondido }}</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Upload:</span>
+                    <ul>
+                        <li v-for="(arquivo, index) in form.arquivos" :key="index">{{ arquivo.name }}</li>
+                    </ul>
+                </div>  
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+import moment from 'moment'
+console.log(moment)
+export default {
+    name: 'ProjetoFormulario',
+    data:() =>({
+        moment:{}, 
+        form:{},
+        formEstadoInicial:{
+        nome: '',
+        email: '',
+        senha: '',
+        idade:'',
+        licenca: 'NÃO',
+        interesses: [],
+        genero: '',
+        telefone:'',
+        dataHoraLocal:'',
+        mes:'',
+        semana:'',
+        hora:'',
+        cor:'#6c757d',
+        alcance: 5,
+        escondido: 'Esse input está escondido',
+        arquivos: {}
+        }
+    }),
+    created(){
+        this.resetar()
+    },
+    methods: {
+        selecionarArquivos(event){
+            console.log(event.target.files)
+            this.form.arquivos = event.target.files
+        },
+        enviar(){
+            const formEnvio = Object.assign({}, this.form)
+            console.log(formEnvio)
+        },
+        resetar(){
+            this.form = Object.assign({}, this.formEstadoInicial) 
+        }   
+
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Maska from 'maska'
+import moment from 'moment'
+
+//createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Maska)
+app.config.globalProperties.$moment = moment
+app.mount('#app')
